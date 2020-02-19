9a8a26524256d8391209a0480566af52bdbd714b
Max Plank <darkhan.r4industry@gmail.com>
2020-02-19 16:18:27 +0600

    starter commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\darkha~1\docume~1\softwa~1\projects\sana_d~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
